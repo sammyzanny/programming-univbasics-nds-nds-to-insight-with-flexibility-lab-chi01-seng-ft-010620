ff93edb10eead6107f8f281a7e3e81b45cd98f65
Samuel Zandi <samuelczandi@gmail.com>
2020-01-02 22:03:04 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -50,7 +50,7 @@ def movies_with_director_key(name, movies_collection)
   # movie_with_director_name method
   i = 0 
    while i < movies_collection.length do 
-     movies_collection[i][:director_name] = name
+     movie_with_director_name(name, movies_collection[i])
      
        i += 1
       end 
